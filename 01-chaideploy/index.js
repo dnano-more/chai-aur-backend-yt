07cf012653a6682222ffc40ff67fc3fddb4736da
Dnyaneshwar More <dnyaneshwarmore2500@gmail.com>
2026-04-13 11:22:49 +0530

    implement express server with multiple routes and basic responses

--- 01-chaideploy/index.js
+++ 01-chaideploy/index.js
@@ -1,1 +1,23 @@
-console.log("chai aur code!")
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/dnano-site", (req, res) => {
+    res.send("This is dnano's porfolio site")
+})
+
+app.get("/html-page", (req, res) => {
+    res.send("<h1>Ham html bhe bhe j sakte hain response me!</h1>")
+})
+
+app.get("/youtube", (req, res) => {
+    res.send('<h2 style="text-transform: capitalize; color: #e67e00;">chai aur backend</h2>');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
