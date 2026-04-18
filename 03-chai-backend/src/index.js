7f40269691d92c09f0a65f3f8ce3f6cbb457c274
Dnyaneshwar More <dnyaneshwarmore2500@gmail.com>
2026-04-18 16:17:07 +0530

    connect to mongodb atlas

--- 03-chai-backend/src/index.js
+++ 03-chai-backend/src/index.js
@@ -1,1 +1,33 @@
-console.log("hello backend!");
+// require("dotenv").config({path: "./env"})
+
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./db/index.js";
+
+dotenv.config();
+
+const app = express();
+
+connectDB();
+
+
+/*
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log("Connected to MongoDB");
+
+    app.on("error", (err) => {
+      console.log("ERROR: ", err);
+      throw err;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
+})();
+*/
